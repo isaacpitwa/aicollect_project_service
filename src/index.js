8a5f88095bd8b8cc5bf3323b7f693307b4ee6dbe
isaacpitwa <isaacpitwa256@gmail.com>
2022-07-07 18:47:59 +0300

    update to incopertate image upload

--- src/index.js
+++ src/index.js
@@ -20,8 +20,8 @@ app.enable('trust proxy');
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb', parameterLimit: 50000 }));
 app.use(session({
   store: '',
   secret: 'secret',
