03c143ea046fdb0f82c3911b9871eb19fe22ab99
Isaac Pitwa <isaacpitwa256@gmail.com>
2022-09-05 14:06:37 +0300

    Adjust staging URL

--- src/index.js
+++ src/index.js
@@ -36,21 +36,21 @@ let MONGO_URI;
 let mongoConfigObject;
 
 if (process.env.NODE_ENV === 'production') {
-  MONGO_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@db-mongodb-fra1-40589-c19b3827.mongo.ondigitalocean.com/aicollect_test?retryWrites=true&w=majority&authSource=admin`;
-  mongoConfigObject = {
-    keepAlive: true,
-    keepAliveInitialDelay: 300000,
-    useNewUrlParser: true,
-    tlsInsecure: true,
-    useUnifiedTopology: true
-  };
-  // MONGO_URI = 'mongodb://64.227.187.135:27017/aicollect';
+  // MONGO_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@db-mongodb-fra1-40589-c19b3827.mongo.ondigitalocean.com/aicollect_test?retryWrites=true&w=majority&authSource=admin`;
   // mongoConfigObject = {
   //   keepAlive: true,
   //   keepAliveInitialDelay: 300000,
   //   useNewUrlParser: true,
+  //   tlsInsecure: true,
   //   useUnifiedTopology: true
   // };
+  MONGO_URI = 'mongodb://64.227.187.135:27017/aicollect';
+  mongoConfigObject = {
+    keepAlive: true,
+    keepAliveInitialDelay: 300000,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  };
 } else {
   MONGO_URI = 'mongodb://localhost:27017/aicollect';
   mongoConfigObject = {
