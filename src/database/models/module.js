86cb22c6cc1726d5135bbe96e3cc76cbbaf948e9
Isaac Pitwa <isaacpitwa256@gmail.com>
2022-10-03 10:34:01 +0300

    Add status to module

--- src/database/models/module.js
+++ src/database/models/module.js
@@ -2,15 +2,17 @@ import { Schema, model } from 'mongoose';
 
 const ModuleSchema = new Schema({
     _id: Types.ObjectId,
-    type: {
-        type: Schema.Types.ObjectId,
-        ref: 'FormType'
-    },
     name: { type: String, required: true },
+    description: { type: String, required: true },
     sector: {
         type: Types.ObjectId,
         ref: 'Sector'
     },
+    type: {
+        type: Schema.Types.ObjectId,
+        ref: 'FormType'
+    },
+    createdBy: { type: Number, required: true },
     status: {
         type: String,
         enum: ['draft', 'published', 'archived'],
